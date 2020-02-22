d4e254c44c5a9e79599b0c671523cfd4b16910f1
hirohaku21 <s1250025@u-aizu.ac.jp>
2020-02-23 01:14:01 +0900

    commit

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var STORAGE_KEY = 'todo-vuejs-demo'
 var todoStorage = {
+
+
   fetch: function(){
     var todos = JSON.parse(
       localStorage.getItem(STORAGE_KEY) || '[]'
@@ -20,11 +22,13 @@ var todoStorage = {
 const app = new Vue({
   el: '#app',
   data: {
-    todos:[
-    // {id: '1',comment: 'aiueo',state:''},
-    // {comment: 'aiueo'},
-    // {state:'maru'}
-    ]
+    todos:[],
+    options:[
+    {value:-1 ,label:'すべて'},
+    {value:0 ,label:'作業中'},
+    {value:1 ,label:'完了'}
+    ],
+    current: -1
   },
   methods: {
     doAdd: function(event, value){
@@ -38,8 +42,33 @@ const app = new Vue({
         state: 0
       })
       comment.value = ''
+    },
+    doChangeState: function(item){
+      item.state = item.state ? 0:1
+    },
+    doRemove: function(item){
+      var index = this.todos.indexOf(item)
+      this.todos.splice(index,1)
+    },
+    editTodo: function(id){
+      var newText = window.prompt('以下の内容で更新します。');
+      if(newText === ''){
+        alert('入力欄が空欄です！');
+      }else if(newText !== ''){
+        this.edit(id, newText);
+      }
+    },
+    edit: function(id, text){
+      var editIndex = '';
+      this.todos.some(function(value,index){
+        if(value.id === id){
+          editIndex = index;
+        }
+      });
+      this.list[editIndex].text = text;
     }
   },
+  
   watch:{
     todos: {
       handler: function(todos){
@@ -47,7 +76,25 @@ const app = new Vue({
       },
       deep: true
     }
+  },
+  
+  created(){
+    this.todos = todoStorage.fetch()
+  },
+
+  computed:{
+    computedTodos: function(){
+      return this.todos.filter(function(el){
+        console.log("hogehoge");
+        return this.current < 0 ? true : this.current === el.state
+      }, this)
+    },
+    labels(){
+      return this.options.reduce(function(a,b){
+        return Object.assign(a, { [b.value]: b.label })
+      },{})
+    }
   }
-  
-  
 })
+
+
